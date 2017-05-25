7a69a6d5e0316b8e2ff5f7d40fd4660313fb9b90
jialeihu <jialeihu2019@u.northwestern.edu>
2017-05-25 16:05:30 -0500

    HW14

--- HW10/firmware/HW10.X/build/default/debug/_ext/1360937237/ILI9163C.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/_ext/1360937237/ILI9163C.o:  \
-../src/ILI9163C.c  \
- ../src/ILI9163C.h 
--- HW10/firmware/HW10.X/build/default/debug/_ext/1360937237/i2c.o.d
+++
@@ -1,4 +0,0 @@
-build/default/debug/_ext/1360937237/i2c.o:  \
-../src/i2c.c  \
-../src/i2c.h  \
- ../src/ILI9163C.h 
