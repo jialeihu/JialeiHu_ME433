0dd729cb2b969678f8efd144498e1c25f96c1db8
jialeihu <jialeihu2019@u.northwestern.edu>
2017-05-25 16:51:09 -0500

    HW14

--- HW14/firmware/src/app.c
+++ HW14/firmware/src/app.c
@@ -54,7 +54,7 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 // *****************************************************************************
 
 #include "app.h"
-#include <stdio.h>
+//#include <stdio.h>
 #include <xc.h>
 
 // *****************************************************************************
